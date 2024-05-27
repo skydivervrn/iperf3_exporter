de9b0a8bf692be7ee399224428011d30c4cda246
aleksandr.boikov <aleksandr.boikov@jetbrains.com>
2024-05-27 21:09:07 +0200

    Updated metrics. Now added all meaningfull metrics from 'end' section of JSON output.

--- main.go
+++ main.go
@@ -15,45 +15,113 @@ import (
 
 var (
 	targetHost = flag.String("target", "", "(Required) Server address to connect to")
+	targetPort = flag.String("p", "5201", "(Optional) Server port to connect to")
 )
 
 type iperf3Collector struct {
-	intervalsStreamsBitsPerSecond *prometheus.Desc
-	intervalsStreamsRetransmits   *prometheus.Desc
-	intervalsStreamsRtt           *prometheus.Desc
-	intervalsStreamsRttvar        *prometheus.Desc
-	intervalsStreamsPmtu          *prometheus.Desc
-	endSumSentSeconds             *prometheus.Desc
-	endSumSentBytes               *prometheus.Desc
+	EndSumSentBitsPerSecond     *prometheus.Desc
+	EndSumReceivedBitsPerSecond *prometheus.Desc
+	EndSumSentBytes             *prometheus.Desc
+	EndSumReceivedBytes         *prometheus.Desc
+	EndSumSentRetransmits       *prometheus.Desc
+	EndStreamsSenderMaxRtt      *prometheus.Desc
+	EndStreamsSenderMinRtt      *prometheus.Desc
+	EndStreamsSenderMeanRtt     *prometheus.Desc
+	EndStreamsSenderMaxSndCwnd  *prometheus.Desc
+	EndStreamsSenderMaxSndWnd   *prometheus.Desc
+}
+
+func (collector *iperf3Collector) Describe(ch chan<- *prometheus.Desc) {
+	//Update this section with the each metric you create for a given collector
+	ch <- collector.EndSumSentBitsPerSecond
+	ch <- collector.EndSumReceivedBitsPerSecond
+	ch <- collector.EndSumSentBytes
+	ch <- collector.EndSumReceivedBytes
+	ch <- collector.EndSumSentRetransmits
+	ch <- collector.EndStreamsSenderMaxRtt
+	ch <- collector.EndStreamsSenderMinRtt
+	ch <- collector.EndStreamsSenderMeanRtt
+	ch <- collector.EndStreamsSenderMaxSndCwnd
+	ch <- collector.EndStreamsSenderMaxSndWnd
+}
+
+// Collect implements required collect function for all promehteus collectors
+func (collector *iperf3Collector) Collect(ch chan<- prometheus.Metric) {
+	ctx, cancel := context.WithTimeout(context.Background(), 4000*time.Second)
+	defer cancel()
+	out, err := exec.CommandContext(ctx, "/usr/bin/iperf3", "-c", *targetHost, "-J", "-p", *targetPort).Output()
+	if err != nil {
+		log.Printf("Failed to run iperf3: %s", err)
+		return
+	}
+	//log.Println(string(out))
+	log.Println("Collecting metrics")
+	stats := iperf3Result{}
+	if err := json.Unmarshal(out, &stats); err != nil {
+		log.Printf("Failed to parse iperf3 result: %s", err)
+		return
+	}
+	EndSumSentBitsPerSecond := prometheus.MustNewConstMetric(collector.EndSumSentBitsPerSecond, prometheus.GaugeValue, stats.End.SumSent.BitsPerSecond)
+	ch <- EndSumSentBitsPerSecond
+	EndSumReceivedBitsPerSecond := prometheus.MustNewConstMetric(collector.EndSumReceivedBitsPerSecond, prometheus.GaugeValue, stats.End.SumReceived.BitsPerSecond)
+	ch <- EndSumReceivedBitsPerSecond
+	EndSumSentBytes := prometheus.MustNewConstMetric(collector.EndSumSentBytes, prometheus.GaugeValue, float64(stats.End.SumSent.Bytes))
+	ch <- EndSumSentBytes
+	EndSumReceivedBytes := prometheus.MustNewConstMetric(collector.EndSumReceivedBytes, prometheus.GaugeValue, float64(stats.End.SumReceived.Bytes))
+	ch <- EndSumReceivedBytes
+	EndSumSentRetransmits := prometheus.MustNewConstMetric(collector.EndSumSentRetransmits, prometheus.GaugeValue, float64(stats.End.SumSent.Retransmits))
+	ch <- EndSumSentRetransmits
+	EndStreamsSenderMaxRtt := prometheus.MustNewConstMetric(collector.EndStreamsSenderMaxRtt, prometheus.GaugeValue, float64(stats.End.Streams[0].Sender.MaxRtt))
+	ch <- EndStreamsSenderMaxRtt
+	EndStreamsSenderMinRtt := prometheus.MustNewConstMetric(collector.EndStreamsSenderMinRtt, prometheus.GaugeValue, float64(stats.End.Streams[0].Sender.MinRtt))
+	ch <- EndStreamsSenderMinRtt
+	EndStreamsSenderMeanRtt := prometheus.MustNewConstMetric(collector.EndStreamsSenderMeanRtt, prometheus.GaugeValue, float64(stats.End.Streams[0].Sender.MeanRtt))
+	ch <- EndStreamsSenderMeanRtt
+	EndStreamsSenderMaxSndCwnd := prometheus.MustNewConstMetric(collector.EndStreamsSenderMaxSndCwnd, prometheus.GaugeValue, float64(stats.End.Streams[0].Sender.MaxSndCwnd))
+	ch <- EndStreamsSenderMaxSndCwnd
+	EndStreamsSenderMaxSndWnd := prometheus.MustNewConstMetric(collector.EndStreamsSenderMaxSndWnd, prometheus.GaugeValue, float64(stats.End.Streams[0].Sender.MaxSndWnd))
+	ch <- EndStreamsSenderMaxSndWnd
 }
 
 func newMetricsCollector() *iperf3Collector {
 	return &iperf3Collector{
-		intervalsStreamsBitsPerSecond: prometheus.NewDesc("iperf3_intervals_streams_bits_per_second",
+		EndSumSentBitsPerSecond: prometheus.NewDesc("iperf3_end_sum_sent_bits_per_second",
 			"Shows TBD",
 			nil, nil,
 		),
-		intervalsStreamsRetransmits: prometheus.NewDesc("iperf3_intervals_streams_retransmits",
+		EndSumReceivedBitsPerSecond: prometheus.NewDesc("iperf3_end_sum_received_bits_per_second",
 			"Shows TBD",
 			nil, nil,
 		),
-		intervalsStreamsRtt: prometheus.NewDesc("iperf3_intervals_streams_rtt",
+		EndSumSentBytes: prometheus.NewDesc("iperf3_end_sum_sent_bytes",
 			"Shows TBD",
 			nil, nil,
 		),
-		intervalsStreamsRttvar: prometheus.NewDesc("iperf3_intervals_streams_rttvar",
+		EndSumReceivedBytes: prometheus.NewDesc("iperf3_end_sum_received_bytes",
 			"Shows TBD",
 			nil, nil,
 		),
-		intervalsStreamsPmtu: prometheus.NewDesc("iperf3_intervals_streams_pmtu",
+		EndSumSentRetransmits: prometheus.NewDesc("iperf3_end_sum_sent_retransmits",
 			"Shows TBD",
 			nil, nil,
 		),
-		endSumSentSeconds: prometheus.NewDesc("iperf3_end_sum_sent_seconds",
+		EndStreamsSenderMaxRtt: prometheus.NewDesc("iperf3_end_streams_sender_max_rtt",
 			"Shows TBD",
 			nil, nil,
 		),
-		endSumSentBytes: prometheus.NewDesc("iperf3_end_sum_sent_bytes",
+		EndStreamsSenderMinRtt: prometheus.NewDesc("iperf3_end_streams_sender_min_rtt",
+			"Shows TBD",
+			nil, nil,
+		),
+		EndStreamsSenderMeanRtt: prometheus.NewDesc("iperf3_end_streams_sender_mean_rtt",
+			"Shows TBD",
+			nil, nil,
+		),
+		EndStreamsSenderMaxSndCwnd: prometheus.NewDesc("iperf3_end_streams_sender_max_snd_cwnd",
+			"Shows TBD",
+			nil, nil,
+		),
+		EndStreamsSenderMaxSndWnd: prometheus.NewDesc("iperf3_end_streams_sender_max_snd_wnd",
 			"Shows TBD",
 			nil, nil,
 		),
@@ -73,47 +141,4 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
-}
-
-func (collector *iperf3Collector) Describe(ch chan<- *prometheus.Desc) {
-	//Update this section with the each metric you create for a given collector
-	ch <- collector.intervalsStreamsBitsPerSecond
-	ch <- collector.intervalsStreamsRetransmits
-	ch <- collector.intervalsStreamsRtt
-	ch <- collector.intervalsStreamsRttvar
-	ch <- collector.intervalsStreamsPmtu
-	ch <- collector.endSumSentSeconds
-	ch <- collector.endSumSentBytes
-}
-
-// Collect implements required collect function for all promehteus collectors
-func (collector *iperf3Collector) Collect(ch chan<- prometheus.Metric) {
-	ctx, cancel := context.WithTimeout(context.Background(), 4000*time.Second)
-	defer cancel()
-	out, err := exec.CommandContext(ctx, "/usr/bin/iperf3", "-c", *targetHost, "-J").Output()
-	if err != nil {
-		log.Printf("Failed to run iperf3: %s", err)
-		return
-	}
-	//log.Println(string(out))
-	log.Println("Collecting metrics")
-	stats := iperf3Result{}
-	if err := json.Unmarshal(out, &stats); err != nil {
-		log.Printf("Failed to parse iperf3 result: %s", err)
-		return
-	}
-	intervalsStreamsBitsPerSecond := prometheus.MustNewConstMetric(collector.intervalsStreamsBitsPerSecond, prometheus.GaugeValue, stats.Intervals[0].Streams[0].BitsPerSecond)
-	ch <- intervalsStreamsBitsPerSecond
-	intervalsStreamsRetransmits := prometheus.MustNewConstMetric(collector.intervalsStreamsRetransmits, prometheus.GaugeValue, float64(stats.Intervals[0].Streams[0].Retransmits))
-	ch <- intervalsStreamsRetransmits
-	intervalsStreamsRtt := prometheus.MustNewConstMetric(collector.intervalsStreamsRtt, prometheus.GaugeValue, float64(stats.Intervals[0].Streams[0].Rtt))
-	ch <- intervalsStreamsRtt
-	intervalsStreamsRttvar := prometheus.MustNewConstMetric(collector.intervalsStreamsRttvar, prometheus.GaugeValue, float64(stats.Intervals[0].Streams[0].Rttvar))
-	ch <- intervalsStreamsRttvar
-	intervalsStreamsPmtu := prometheus.MustNewConstMetric(collector.intervalsStreamsPmtu, prometheus.GaugeValue, float64(stats.Intervals[0].Streams[0].Pmtu))
-	ch <- intervalsStreamsPmtu
-	endSumSentSeconds := prometheus.MustNewConstMetric(collector.endSumSentSeconds, prometheus.GaugeValue, stats.End.SumSent.Seconds)
-	ch <- endSumSentSeconds
-	endSumSentBytes := prometheus.MustNewConstMetric(collector.endSumSentBytes, prometheus.GaugeValue, stats.End.SumSent.BitsPerSecond)
-	ch <- endSumSentBytes
 }
